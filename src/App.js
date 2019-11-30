4f4c71fa732c51279db41a8def5821527849660b
hahalong <2072105669@qq.com>
2019-11-30 14:00:05 +0800

    修改提交

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import {nsg,my} from './api'
 import homenv from '~/homenv';
 import homecb from '~/homecb';
 import Inf from '~/Inf';
+import Foundmore from '~/Foundmore';
 
 class App extends Component {
   
@@ -45,6 +46,8 @@ class App extends Component {
                 <Route path="/detail" component={Detail} />
                 <Route path="/reward" component={Reward} />
                 <Route path="/rank" component={Rank} />
+                <Route path="/Foundmore" component={Foundmore} />
+                
                 <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal" className="nav">
                     
                     <Menu.Item key="book">
